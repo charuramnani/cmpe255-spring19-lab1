b89eb957eb74024766b6672bff4140e5e494073e
charuramnani <35973040+charuramnani@users.noreply.github.com>
2019-02-07 16:51:24 -0800

    Create friends.py

---
+++ friends.py
@@ -0,0 +1,58 @@
+users =[
+    { "id":0, "name": "Hero" },
+    { "id":1, "name": "Dunn" },
+    { "id":2, "name": "Sue" },
+    { "id":3, "name": "Chi" },
+    { "id":4, "name": "Thor" },
+    { "id":5, "name": "Clive" },
+    { "id":6, "name": "Hicks" },
+    { "id":7, "name": "Devin" },
+    { "id":8, "name": "Kate" },
+    { "id":9, "name": "Klein" }    
+]
+
+friendship = [
+    (0, 1),
+    (0, 2),
+    (1, 2),
+    (1, 3),
+    (2, 3),
+    (3, 4),
+    (4, 5),
+    (5, 6),
+    (6, 7),
+    (6, 8),
+    (7, 8),
+    (8, 9)
+]
+for user in users:
+    user["friends"] = []
+
+
+for i, j in friendship:
+    
+    users[i]["friends"].append(users[j]) 
+    users[j]["friends"].append(users[i]) 
+
+def num_friends(user):
+    '''
+    Find number of friends for a given user
+    '''
+    return len(user["chi"]) # length of friend_ids list
+    pass
+            
+ total_connections = sum(num_friends(user)
+                        for user in users) 
+
+print(total_connections)
+
+def sort_by_num_friends():
+    '''README.md
+    Sort from "most friends" to "least friends"
+    '''
+    num_friends_by_id = [(user["id"], num_friends(user))
+                         for user in users]
+    
+    pass
+ 
+ sorted(num_friends_by_id)
